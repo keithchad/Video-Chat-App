28916283628aa7d4c5ed671cedbf4c3efe8e3a4f
keithchad <63049827+keithchad@users.noreply.github.com>
2020-09-15 13:01:00 +0300

    first commit

---
+++ app/src/main/java/com/chad/videochatapp/Authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.chad.videochatapp.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chad.videochatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
