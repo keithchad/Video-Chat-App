d08c69211e3b6c3e48d3421f3b67c91fb445a522
keithchad <63049827+keithchad@users.noreply.github.com>
2020-09-29 05:30:52 +0300

    Testing Placement of Child Fragment

--- app/src/main/java/com/chad/videochatapp/Fragments/CreateMeetingFragment.java
+++ app/src/main/java/com/chad/videochatapp/Fragments/CreateMeetingFragment.java
@@ -14,21 +14,61 @@ import com.chad.videochatapp.R;
 
 public class CreateMeetingFragment extends Fragment{
 
-   private TextView createMeeting;
-
+    private TextView createMeeting;
+    private TextView joinMeeting;
+    private Fragment selectedFragment = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        FragmentManager childFragment = getChildFragmentManager();
-        FragmentTransaction childFragmentTransaction = childFragment.beginTransaction();
-        SettingsFragment settingsFragment = new SettingsFragment();
-        childFragmentTransaction.add(R.id.meetingContainer, settingsFragment);
-        childFragmentTransaction.commit();
+        createMeeting = view.findViewById(R.id.createMeetingText);
+        joinMeeting = view.findViewById(R.id.joinMeetingText);
+
+        createMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(v);
+            }
+        });
+
+
+        joinMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(v);
+            }
+        });
+
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.meetingContainer, new SettingsFragment()).commit();
+
+//        FragmentManager childFragment = getChildFragmentManager();
+//        FragmentTransaction childFragmentTransaction = childFragment.beginTransaction();
+//        SettingsFragment settingsFragment = new SettingsFragment();
+//        childFragmentTransaction.add(R.id.meetingContainer, settingsFragment);
+//        childFragmentTransaction.commit();
 
        return view;
+    }
+
+    private void onButtonClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.createMeetingText :
+                selectedFragment = new SettingsFragment();
+                break;
+
+            case R.id.joinMeetingText :
+                selectedFragment = new FriendsFragment();
+                break;
+        }
+
+        if (selectedFragment != null) {
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.meetingContainer, selectedFragment).commit();
+        }
     }
 
 }
